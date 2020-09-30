d50a692430e1dce34f68f171edd0eb3857c26e87
Alberto Marín González <alberto.marin@imatiasl.lan>
2020-09-30 12:54:22 +0200

    Recent Transactions

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 
 import './widgets/new_transaction.dart';
 
+import './widgets/chart.dart';
+
 import './models/transaction.dart';
 
 void main() {
@@ -68,6 +70,16 @@ class _MyHomePageState extends State<MyHomePage> {
     // ),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((transaction) {
+      return transaction.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTransaction = Transaction(title: txTitle, amount: txAmount, date: DateTime.now(), id: DateTime.now().toString());
     setState(() {
@@ -114,6 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 elevation: 5,
               ),
             ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
